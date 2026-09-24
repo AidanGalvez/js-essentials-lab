e9f9ab999cbed967afbe1e04a6a6c13ed576980d
AidanMGalvez <aidang9728@gmail.com>
2026-09-24 11:38:00 -0600

    Todo-Checklist

---
+++ todo-checklist/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import TodoInput from './components/TodoInput';
+import styles from './App.module.css';
+
+const App = () => {
+  const [todoItems, setTodoItems] = useState([
+    { id: 't1', text: 'Finish homework' },
+    { id: 't2', text: 'Go on a hike' }
+  ]);
+
+  const addTodoHandler = enteredText => {
+    setTodoItems(prevTodos => [
+      { id: Math.random().toString(), text: enteredText },
+      ...prevTodos
+    ]);
+  };
+
+  const deleteItemHandler = todoId => {
+    setTodoItems(prevTodos => prevTodos.filter(todo => todo.id !== todoId));
+  };
+
+  return (
+    <div className={styles["app-container"]}>
+      <section className ={styles["todo-form"]}>
+        <TodoInput onAddTodo={addTodoHandler} />
+      </section>
+      <section className ={styles.todos}>
+        <ul>
+          {todoItems.map(todo => (
+            <li className = {styles.li} key={todo.id} onClick={() => deleteItemHandler(todo.id)}>
+              {todo.text}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default App;
---
+++ todo-checklist/src/components/Button.jsx
@@ -0,0 +1,31 @@
+//import './Button.css';
+//import styled from "styled-components"
+import styles from './Button.module.css'
+/*
+const Button = styled.button`
+
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: none;
+  background-color: #492365;
+  color: #ffffff;
+  cursor: pointer;
+
+
+&:hover,
+&:active {
+  background-color: #36194b;
+}
+`;
+*/
+
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+
+export default Button;
