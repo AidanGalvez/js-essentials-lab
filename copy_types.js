48624b2720be2c1af561ff908c7cb613b6243f62
AidanGalvez <112118883+AidanGalvez@users.noreply.github.com>
2026-09-24 10:44:08 -0600

    Delete copy_types.js

--- copy_types.js
+++
@@ -1,7 +0,0 @@
-const user = { name: "Aidan", age: 21, fruits: ["apple", "banana", "oranges"]};
-const copyUser = {...user};
-
-copyUser.name = "Token";
-copyUser.fruits[0] = "watermelon";
-console.log(user);
-console.log(copyUser);
